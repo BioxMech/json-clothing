cb4a206a4f94a3817b349c69a71b48086ba110bb
Jason <livelivelivelive12345@gmail.com>
2021-01-03 16:35:11 +0800

    Cart redux made + toggle icon

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReduceer from './cart/cart.reducer'
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReduceer
 })
